90c5c2203a66ae7f5b3c0ff2bca066bffff2b1fa
Carrie Jones <c.jessica.jones@gmail.com>
2020-05-23 16:41:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,7 +92,7 @@ def won?(board)
     position_2 = board[win_index2]
     position_3 = board[win_index3]
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index1)
-      return win_combination
+      return true
     end
   end
   nil
